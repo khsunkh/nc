3f219c7cec876861fad645288634803d847dba5d
khsunkh <khsunkh@gmail.com>
2021-09-22 02:10:47 +0000

    2.2 Dynamic Component Generation

--- 02_reactjs_movie_app/src/App.js
+++ 02_reactjs_movie_app/src/App.js
@@ -6,23 +6,52 @@ import React from 'react'
 //     <h1> I like {props.title}</h1>
 //   );
 // }
+const movieList = [
+  {
+    title: "Run",
+    date: "2020",
+    image: "https://upload.wikimedia.org/wikipedia/en/4/45/Run_poster.jpeg"
+  },
+  {
+    title: "The One",
+    date: "2001",
+    image: "https://upload.wikimedia.org/wikipedia/en/f/fc/TheOnefilm.jpg"
+  },
+  {
+    title: "The Show",
+    date: "2017",
+    image: "https://upload.wikimedia.org/wikipedia/en/8/88/The_Show_2017_poster.jpg"
+  }
+]
 
-function Movie( { date, title } ) {
+function Movie( { title, date, image } ) {
   return (
-    <h1> I like {title}, {date} </h1>
+    <div>
+      <h1>Title: {title}</h1>
+      <h2>Date : {date}</h2>
+      <img src={image} />
+    </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>GHello!!!!!</h1>
+//       <Movie title="Run" date="2021.01.03"/>
+//       <Movie title="The One" date="2021.01.04"/>
+//       <Movie title="Last Kingdom" date="2021.01.05"/>
+//       <Movie title="Show" date="2021.01.06"/>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div>
       <h1>GHello!!!!!</h1>
-      <Movie title="Run" date="2021.01.03"/>
-      <Movie title="The One" date="2021.01.04"/>
-      <Movie title="Last Kingdom" date="2021.01.05"/>
-      <Movie title="Show" date="2021.01.06"/>
+      {movieList.map( movie => <Movie title={movie.title} date={movie.date} image={movie.image}/> )}
     </div>
   );
 }
-
 export default App;
