9f02f388a0e02d4d3ea2a0584388a53394ba01aa
khsunkh <khsunkh@gmail.com>
2021-09-22 01:47:55 +0000

    reusable components with jsx + props

--- 02_reactjs_movie_app/src/App.js
+++ 02_reactjs_movie_app/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import Potato from './Potato';
+
+
+// function Movie(props) {
+//   return (
+//     <h1> I like {props.title}</h1>
+//   );
+// }
+
+function Movie( { date, title } ) {
+  return (
+    <h1> I like {title}, {date} </h1>
+  );
+}
 
 function App() {
   return (
     <div>
       <h1>GHello!!!!!</h1>
-      <Potato />
+      <Movie title="Run" date="2021.01.03"/>
+      <Movie title="The One" date="2021.01.04"/>
+      <Movie title="Last Kingdom" date="2021.01.05"/>
+      <Movie title="Show" date="2021.01.06"/>
     </div>
   );
 }
